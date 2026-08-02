b328dacd8a1edd8326e0f264b9f4ab93fdaaed1d
Jessie's Shelf <josesavio2008@gmail.com>
2026-08-02 20:14:06 -0300

    site again

--- discord/test_sheets.py
+++
@@ -1,5 +0,0 @@
-import gspread
-
-gs = gspread.service_account(r'D:\Coders\rewritten\rewritten-504216-8ed7e75237c4.json')
-sheet = gs.open("Rewritten Fichas").worksheet("Miller")
-print(sheet.get("I8:I13"))
